c8ed701a3bdb59dfbd15656150abd2717544da09
Harena0712 <harenasarmnk@gmail.com>
2025-08-21 16:21:57 +0300

    index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>index</title>
+</head>
+<body class="container">
+    <h1 class="text-center mt-4">Login</h1>
+    <div class="bg-light col-md-5 rounded mt-4 text-center">
+        <h4 class="text-center p-3">Site emprunt</h4>
+        <form action="page/traitement_login.php" method="post">
+            <input type="texte" class="col-md-10 p-1 rounded" name="" id="" placeholder="   email">
+            <input type="password" class="col-md-10 p-1 rounded mt-4" name="" id="" placeholder="   mot de passe">
+            <input type="submit" class="col-md-7 p-1 rounded mt-4 mb-4" value="Se connecter">
+        </form>
+        <hr>
+        <a href="pages/inscription.php">S'inscrire</a>
+    </div>
+</body>
+</html>
